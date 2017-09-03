792bdf39d3cde9469b607c8338863694d4100ca8
DESKTOP-7BEQCBT\Bill <baotuan0.0@gmail.com>
2017-09-03 14:13:12 +0700

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('HELLO');
+    res.send({hello: "Hello world!"});
 })
 
 const PORT = process.env.PORT || 5000;
