18794578a973a8795509e9c4e91f0432e676f406
DESKTOP-7BEQCBT\Bill <baotuan0.0@gmail.com>
2017-09-05 23:25:02 +0700

    Finished OAuth with Google

---
+++ services/passport.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const key = require("../config/key");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((id, done) => {
+  done(null, id);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: key.googleClientID,
+      clientSecret: key.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accesssToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // TODO: Already have user
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
