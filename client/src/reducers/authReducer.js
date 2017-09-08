b4319b25d1d1914ea6acc706452c3927adc0513b
DESKTOP-7BEQCBT\Bill <baotuan0.0@gmail.com>
2017-09-08 12:34:52 +0700

    Added action creactor for get auth api

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
 const initialState = {};
 
 export default function(state = initialState, action) {
+  console.log("====================================");
+  console.log(action);
+  console.log("====================================");
   switch (action.type) {
     default:
       return state;
