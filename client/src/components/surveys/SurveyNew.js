421fcb350278d19aac312daaae72b166674b5472
DESKTOP-7BEQCBT\Bill <baotuan0.0@gmail.com>
2017-09-10 22:45:40 +0700

    Added sample form with redux-form

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
+
+class SurveyNew extends Component {
+  state = {};
+  render() {
+    return <SurveyForm />;
+  }
+}
+
+export default SurveyNew;
